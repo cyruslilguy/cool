6b320857b372bbad806460fdf8c25a2c8797b9c5
cyruslilguy <94306125+cyruslilguy@users.noreply.github.com>
2023-10-07 20:24:47 -0400

    Update and rename sandboxelsmod.js to superheater_tool.js

---
+++ superheater_tool.js
@@ -0,0 +1,11 @@
+elements.superheater = {
+        color: "#ff0000",
+    behavior: [
+        "HT:2|HT:2|HT:2",
+        "HT:2|HT:2|HT:2",
+        "HT:2|HT:2|HT:2",
+    ],
+    temp: 7000,
+    category: "tools",
+    insulate:true
+};
