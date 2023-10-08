bdc7310465e628814ea2f2ef57434cd72aab5aa4
cyruslilguy <94306125+cyruslilguy@users.noreply.github.com>
2023-10-07 20:14:42 -0400

    Update sandboxelsmod.js

--- sandboxelsmod.js
+++ sandboxelsmod.js
@@ -1,6 +1,9 @@
-elements.disintegrator = {
+elements.ashify = {
     color: "#ff0000",
     tool: function(pixel) {
+        if (pixel.element == "air") {
+            
+        } else {
             pixel.element = "ash"
         }
     },
